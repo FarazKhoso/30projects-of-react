945f0252db2c3f7802e599037a24554b7fab870a
Faraz <161611764+FarazKhoso@users.noreply.github.com>
2024-09-03 09:28:30 -0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import CounterTimer from './component/counter-timer/CounterTimer';
+
+
+function App() {
+  return (
+    <div className="App">
+   <CounterTimer />
+    </div>
+  );
+}
+
+export default App;
